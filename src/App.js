24e36a758a151526eef4aeff760c2ff3eb094853
fadela <f.barghi77@gmail.com>
2020-04-10 22:20:14 +0430

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Home from "./pages/home/Home";
+import Favourite from './pages/favourite/Favourite';
+import Setting from './pages/setting/Setting';
+import Search from './pages/search/Search';
+import { PageContext } from './store/PageContext';
+import Navbar from './components/navbar/Navbar';
+import "./style/main.scss";
 
 function App() {
+
+  const [page, setPage]= useState({number:0});
+
+  const generatePage = (page) => {
+    switch (page.number){
+      case 0:
+        return(
+          <Home/>
+        );
+      case 1:
+        return(
+          <Favourite/>
+        );
+      case 2:
+        return(
+          <Setting/>
+        );
+      case 3:
+        return(
+          <Search/>
+        );
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageContext.Provider value={{page, setPage}}>
+      <React.Fragment>
+        {
+            generatePage(page)
+        }
+        <Navbar/>
+      </React.Fragment>
+    </PageContext.Provider>
   );
 }
 

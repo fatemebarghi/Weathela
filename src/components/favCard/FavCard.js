0a017874aaa371687c3241a91ba89d428921283a
fadela <f.barghi77@gmail.com>
2020-04-20 18:23:05 +0430

    complete favourites api call

--- src/components/favCard/FavCard.js
+++ src/components/favCard/FavCard.js
@@ -1,29 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import "./favCard.scss";
+import {PageContext} from '../../store/PageContext';
 import WeatherIcon from './../weatherIcon/WeatherIcon';
 
 function FavCard (props) {
+
+    const [favResult, setFavResult]= useState(null);
+    const {page, setPage} = useContext(PageContext);
+
+    useEffect( () => {
+        const axios = require('axios');
+        axios.get(`/api/location?lat=${props.data.latitude}&long=${props.data.longitude}`)
+        .then( response => {
+            console.log("in the favourite",response.data);
+            setFavResult(response.data);
+        })
+        .catch( error => {
+            console.log(error);
+        })
+    },[])
+
+    const handleClick = (location) => {
+        console.log("hiii", location)
+        setPage({number: 0});
+        localStorage.setItem("location", JSON.stringify(location));
+    }
     
-    return(
-        <div className="card">
-            {console.log('props in fav card',props.data)}
-            <div className="weather-icon">
-                <WeatherIcon weather={props.data.weatherData.icon}/>
-            </div>
-            <span className="city-title">{props.data.locationData.county}</span>
-            <span className="temp">{Math.round(props.data.weatherData.temperature)}</span>
-            <div className="weather">
-                <div>
-                    <span className="icon-down"></span>
-                    <span className="min-temp">{Math.round(props.data.weatherData.lowestTemp)}</span>
-                </div>
-                <div>
-                    <span className="icon-up"></span>
-                    <span className="max-temp">{Math.round(props.data.weatherData.highestTemp)}</span>
-                </div>
-            </div>
+    return (
+        <div className="card" onClick={() => handleClick(props.data)}>
+            {favResult ?
+                <React.Fragment>
+                    <div className="weather-icon">
+                    <WeatherIcon weather={favResult.weatherData.icon}/>
+                    </div>
+                    <span className="city-title">{favResult.locationData.county || favResult.locationData.city}</span>
+                    <span className="temp">{Math.round(favResult.weatherData.temperature)}</span>
+                    <div className="weather">
+                        <div>
+                            <span className="icon-down"></span>
+                            <span className="min-temp">{Math.round(favResult.weatherData.lowestTemp)}</span>
+                        </div>
+                        <div>
+                            <span className="icon-up"></span>
+                            <span className="max-temp">{Math.round(favResult.weatherData.highestTemp)}</span>
+                        </div>
+                    </div>
+                </React.Fragment>
+            :<div>LOADING....</div>
+            }
         </div>
-    )
+    )                
 }
 
 export default FavCard;
--- src/pages/favourite/Favourite.js
+++ src/pages/favourite/Favourite.js
@@ -4,44 +4,37 @@ import "./favourite.scss";
 
 function Favourite() {
 
-    const [favData, setFavData] = useState(null);
+    // const [favData, setFavData] = useState(null);
     const favLocation = localStorage.getItem("favLocations") ? JSON.parse(localStorage.getItem("favLocations")) : [];
 
-    useEffect( () => {
+    // useEffect( () => {
 
-        let arrLocations= [];
-        if (favLocation.length !==0) {
-            for(let key in favLocation) {
-                console.log("fav", favLocation[key]);
-                const axios = require('axios');
-                axios.get(`/api/location?lat=${favLocation[key].latitude}&long=${favLocation[key].longitude}`)
-                .then( response => {
-                    console.log("in the favourite",response.data);
-                    arrLocations.push(response.data);
-                    console.log("arrLocations",arrLocations);
-                })
-                .catch( error => {
-                    console.log(error);
-                })
-            }
-        }
-        if (!favData) {
-            setFavData(arrLocations);
-        }
-    },[])
+    //     let arrLocations= [];
+    //     if (favLocation.length !==0) {
+    //         for(let key in favLocation) {
+    //             console.log("fav", favLocation[key]);
+    //             const axios = require('axios');
+    //             axios.get(`/api/location?lat=${favLocation[key].latitude}&long=${favLocation[key].longitude}`)
+    //             .then( response => {
+    //                 console.log("in the favourite",response.data);
+    //                 arrLocations.push(response.data);
+    //                 console.log("arrLocations",arrLocations);
+    //             })
+    //             .catch( error => {
+    //                 console.log(error);
+    //             })
+    //         }
+    //     }
+       
+    // },[])
 
     return (
         <div className="favourite-page">
             {
                 favLocation.length !==0 ?
-                    
-                    favData ?
-                        console.log("favdata", favData)
-                        // favData.map((data, index) => (
-                        //     console.log("dataaa", data),
-                        //     <FavCard data={data} key={index}/>
-                        // ))
-                    :<div>LOADING....</div>
+                    favLocation.map((locaion, index) => (
+                        <FavCard data={locaion} key={index}/>
+                    ))
                 : <span className="no-item">هیچ موردی یافت نشد</span>
             }
         </div>

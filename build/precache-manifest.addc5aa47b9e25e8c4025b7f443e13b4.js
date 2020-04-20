e6fb5f5867c1af3c3120ba42e56de7de66ac9a4c
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2020-04-21 00:46:59 +0430

    heroku config

---
+++ build/precache-manifest.addc5aa47b9e25e8c4025b7f443e13b4.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187f0aaa5e4cb626a4cef13b18b49caa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a12fd674664c0d3d15a",
+    "url": "/static/css/main.746e5c94.chunk.css"
+  },
+  {
+    "revision": "f0530f436948b7506808",
+    "url": "/static/js/2.7498cd68.chunk.js"
+  },
+  {
+    "revision": "c601be00a5f629f0cb0a95b6d13cdcf9",
+    "url": "/static/js/2.7498cd68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a12fd674664c0d3d15a",
+    "url": "/static/js/main.0be7e144.chunk.js"
+  },
+  {
+    "revision": "883ec0ddee85aacf583d",
+    "url": "/static/js/runtime-main.c976f3c8.js"
+  },
+  {
+    "revision": "244a401eb43afb0fe739f8dcd8530ae4",
+    "url": "/static/media/IRANSansWeb.244a401e.ttf"
+  },
+  {
+    "revision": "50f4bba2f37081daeca8c423abdced8b",
+    "url": "/static/media/IRANSansWeb.50f4bba2.woff"
+  },
+  {
+    "revision": "70a5f97c18c45e8ef620a374be61c6c3",
+    "url": "/static/media/IRANSansWeb.70a5f97c.eot"
+  },
+  {
+    "revision": "44878c1837bf6f10fcc64801b6d6bc69",
+    "url": "/static/media/Weather_MenuBar_Search-Filled.44878c18.svg"
+  },
+  {
+    "revision": "0d4d2182daa3466090d77f7632abdf7b",
+    "url": "/static/media/fontello.0d4d2182.woff"
+  },
+  {
+    "revision": "20a4f2355e20c4c77345b63d1939cbda",
+    "url": "/static/media/fontello.20a4f235.ttf"
+  },
+  {
+    "revision": "3af4cf3e68b311b0bad89a987b406747",
+    "url": "/static/media/fontello.3af4cf3e.eot"
+  },
+  {
+    "revision": "4785957860fee1afd9f12fd8bd1fb00f",
+    "url": "/static/media/fontello.47859578.woff2"
+  },
+  {
+    "revision": "4a9bdcf0d1f1f66ca1401611ca2ad828",
+    "url": "/static/media/fontello.4a9bdcf0.svg"
+  },
+  {
+    "revision": "27dbc7c8c2004657dc7680e2cd5bf328",
+    "url": "/static/media/state_alborz.27dbc7c8.svg"
+  },
+  {
+    "revision": "2dffeb9f24a4532006e376f206aac844",
+    "url": "/static/media/state_ardabil.2dffeb9f.svg"
+  },
+  {
+    "revision": "f824d93dc50fb4b6d7bd42427f00f415",
+    "url": "/static/media/state_azarbaijan_gharbi.f824d93d.svg"
+  },
+  {
+    "revision": "a11792c10a1f1f799e5c54bae562e484",
+    "url": "/static/media/state_azarbaijan_sharghi.a11792c1.svg"
+  },
+  {
+    "revision": "5c99eed6741ba306ae49b3e8e0dbcf94",
+    "url": "/static/media/state_booshehr.5c99eed6.svg"
+  },
+  {
+    "revision": "3c9a1a7849d8343f7be29d95598f174e",
+    "url": "/static/media/state_chahrmahl_bakhtiarii.3c9a1a78.svg"
+  },
+  {
+    "revision": "2bae8b192649b50796824ca31d382eef",
+    "url": "/static/media/state_esfahan.2bae8b19.svg"
+  },
+  {
+    "revision": "8f9b180d66b00812c954b5b97b056b92",
+    "url": "/static/media/state_ghom.8f9b180d.svg"
+  },
+  {
+    "revision": "3a2fcece632aa2ffdfebdd90d6000083",
+    "url": "/static/media/state_gilan.3a2fcece.svg"
+  },
+  {
+    "revision": "dcf9cec3c4f21b434390bd2dc431ef11",
+    "url": "/static/media/state_golestan.dcf9cec3.svg"
+  },
+  {
+    "revision": "6e6ab757a926d3129ef9ce8baedde048",
+    "url": "/static/media/state_hamedan.6e6ab757.svg"
+  },
+  {
+    "revision": "07b48dcc135beed6274e8a408d455fbc",
+    "url": "/static/media/state_hormozgan.07b48dcc.svg"
+  },
+  {
+    "revision": "de5c03e09362e39cc6d0be401a84f14e",
+    "url": "/static/media/state_kerman.de5c03e0.svg"
+  },
+  {
+    "revision": "bedd57fb70ecedacea57d1e8d2a8b520",
+    "url": "/static/media/state_kermanshah.bedd57fb.svg"
+  },
+  {
+    "revision": "780a009f5b1e24390402c2719357ece5",
+    "url": "/static/media/state_khoozestan.780a009f.svg"
+  },
+  {
+    "revision": "19a09b7b61718226eefb753ab71e4078",
+    "url": "/static/media/state_khorasan_jonobi.19a09b7b.svg"
+  },
+  {
+    "revision": "56a825debb81ab9faff7188968f19da7",
+    "url": "/static/media/state_khorasanrazavi.56a825de.svg"
+  },
+  {
+    "revision": "5fd071d0e097668635e0af117e2ed963",
+    "url": "/static/media/state_kohgilooye.5fd071d0.svg"
+  },
+  {
+    "revision": "edf2d20c459bd6ba777ddbe616904cc8",
+    "url": "/static/media/state_kordestan.edf2d20c.svg"
+  },
+  {
+    "revision": "9bf13fa735d31f719420e8b9a148b515",
+    "url": "/static/media/state_lorestan.9bf13fa7.svg"
+  },
+  {
+    "revision": "8a062ba52b42a7ce7568bcb5c2511d79",
+    "url": "/static/media/state_markazi.8a062ba5.svg"
+  },
+  {
+    "revision": "aad384681ddbf1a1ca5e592f97e2e762",
+    "url": "/static/media/state_mzandaran.aad38468.svg"
+  },
+  {
+    "revision": "de41355d5ae6239c72abc1a5fd428626",
+    "url": "/static/media/state_qazvin.de41355d.svg"
+  },
+  {
+    "revision": "be605f35c112f0470370b226cc57398c",
+    "url": "/static/media/state_shiraz.be605f35.svg"
+  },
+  {
+    "revision": "5468ab229dfcbdb8d14dd2d1f48fa0fb",
+    "url": "/static/media/state_sistan_balochestan.5468ab22.svg"
+  },
+  {
+    "revision": "45408d634ea50179136ef2f11bb89cd7",
+    "url": "/static/media/state_tehran.45408d63.svg"
+  },
+  {
+    "revision": "1ba1323b33c40c09fd1704193b0c9286",
+    "url": "/static/media/state_yazd.1ba1323b.svg"
+  }
+]);

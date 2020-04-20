c7b1d8c8f83dabf68025fc643ac9a61414151ddb
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2020-04-21 01:19:17 +0430

    deploy test

--- server/index.js
+++ server/index.js
@@ -4,10 +4,13 @@ var app = express();
 var apiRouter = require("./api");
 
 
-app.use(express.static('build'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use('/api/', apiRouter);
+// app.use(express.static('build'));
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use('/api/', apiRouter);
+app.get('/', function (req, res) {
+res.send(JSON.stringify({ Hello: 'World'}));
+});
 
 
 app.listen(3001, function () {
